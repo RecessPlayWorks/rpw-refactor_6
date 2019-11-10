fdc24d96cfae0e71e4f23a492b0b52b36845ba7f
Beau Jackson <beau.html@gmail.com>
2019-11-10 13:01:33 -0500

    fixed links to NavLinks

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import NavLink from './subcomponents/NavLink';
+import NavLink from './subComponents/NavLink';
 
 import headerStyles from './header.module.scss';
 
@@ -45,43 +45,3 @@ const Header = () => {
 }
 
 export default Header;
-
-
-/*
-from live-site 
-
-
-import React from 'react';
-import BurgerMenu from './subcomponents/BurgerMenu';
-import NavLink from './subcomponents/NavLink';
-
-const Header = () => (
-  <header className="menu-header">
-    <nav className="home-link">
-      <ul>
-        <li>
-          <NavLink to="/" activeClassName="" textContent="RecessPlayWorks" />
-        </li>
-      </ul>
-    </nav>
-    <nav
-      className="nav-menu">
-      <ul>
-        <li>
-          <NavLink to="/" activeClassName="current-page" textContent="Home" />
-        </li>
-         <li>
-          <NavLink to="/about/" activeClassName="current-page" textContent="About" />
-        </li> 
-        <li>
-          <NavLink to="/contact/" activeClassName="current-page" textContent="Contact" />
-        </li>
-      </ul>
-    </nav>
-    <BurgerMenu />
-
-  </header>
-);
-
-export default Header;
-*/
