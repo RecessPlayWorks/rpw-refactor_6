8ffb6e6877a6671710d4c172020e99947d0d2139
Beau Jackson <beau.html@gmail.com>
2019-11-12 15:24:47 -0500

    adds thanks.js

---
+++ src/pages/thanks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => (
+    <div>
+        <h1>Thank you!</h1>
+        <Link to="/">&larr; back to home</Link>
+    </div>
+)
