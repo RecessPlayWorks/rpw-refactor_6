fc79c0c77bb018ab50e0fefaf7c006ce04aa306f
Beau Jackson <beau.html@gmail.com>
2019-11-15 12:00:56 -0500

    fixed dog problems

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 
 import { Link } from 'gatsby'
 
-import fourohfourDog from '../images/dog.webp';
+import fourohfourDog from '../images/doglist/404-dog3.gif';
 
 import fourOhFourStyles from './fourohfour.module.scss'
 
@@ -14,7 +14,10 @@ const NotFound= () => (
         <h1>404</h1>
         <h2>NOT FOUND</h2>
         <img src={fourohfourDog} alt="dog" />
-        <p>Well, this is awkward. You've arrived at a place that doesn't exist.</p>
+        <p>Well, this is awkward...</p>
+        <p>You've arrived at a place that doesn't exist.</p>
+        <p>If you there is an issue, please don't hesitate to contact us and we'll get back to you.</p>
+
         <button className={fourOhFourStyles.fourOhFourBtn}>
           <Link to="/contact/">Contact</Link>
         </button>
